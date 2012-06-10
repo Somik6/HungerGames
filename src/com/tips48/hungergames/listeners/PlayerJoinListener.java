9461bd506813b32b5c43388f26ed2666ccf4a684
tips48 <amatzko48@gmail.com>
2012-06-10 12:44:09 -0400

    Cleanup ItemGiver, merge

--- src/com/tips48/hungergames/listeners/PlayerJoinListener.java
+++ src/com/tips48/hungergames/listeners/PlayerJoinListener.java
@@ -1,10 +1,12 @@
 package com.tips48.hungergames.listeners;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
 
 import com.tips48.hungergames.GameSession;
 import com.tips48.hungergames.HungerGames;
@@ -32,9 +34,10 @@ public class PlayerJoinListener implements Listener {
 		GameSession session = plugin.getGameManager().getGameSession();
 		Player player = event.getPlayer();
 		session.addPlayer(player);
-		player.teleport(new Location(player.getWorld(), 0, player.getWorld()
-				.getHighestBlockYAt(0, 0), 0)); // TODO replace with arena
-												// locations
+		
+		player.teleport(new Location(player.getWorld(), 0, 0, 0)); // Temporary
+		player.getInventory().clear();
+		player.getInventory().addItem(new ItemStack(Material.STICK, 1));
 	}
 
 }
