f852c9a625fd3ebe31b1244db8b3bf7b4b0e8c8e
tips48 <amatzko48@gmail.com>
2012-06-10 12:44:52 -0400

    I said it worked? No, now it works

---
+++ src/com/tips48/hungergames/dynamic/ItemGiver.java
@@ -0,0 +1,71 @@
+package com.tips48.hungergames.dynamic;
+
+import java.util.Random;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import com.tips48.hungergames.HungerGames;
+
+public class ItemGiver {
+	private HungerGames plugin;
+	private int itemGiveChance;
+	private Random rand;
+
+	/**
+	 * Creates a new ItemGiver
+	 * 
+	 * @param plugin
+	 *            Plugin instance
+	 */
+	public ItemGiver(HungerGames plugin) {
+		this.plugin = plugin;
+		this.rand = new Random();
+		this.itemGiveChance = ((this.rand.nextInt(50) + 25) * 1234);
+	}
+
+	/**
+	 * Handles a PlayerMoveEvent
+	 * 
+	 * @param player
+	 *            Player who moved
+	 */
+	public void handlePlayerMoveEvent(Player player) {
+		if (shouldGiveItem(player.getName())) {
+			player.getInventory().addItem(getItem());
+		}
+	}
+
+	/**
+	 * Checks if an item should be given
+	 * 
+	 * @param name
+	 *            Players name
+	 * @return Item should be given
+	 */
+	private boolean shouldGiveItem(String name) {
+		return ((rand.nextInt(itemGiveChance) * name.length()) == 0);
+	}
+
+	/**
+	 * Gets the next item that should be given
+	 * 
+	 * @return Next item
+	 */
+	private ItemStack getItem() {
+		if (rand.nextBoolean()) {
+			if (rand.nextBoolean()) {
+
+			} else {
+
+			}
+		} else {
+			if (rand.nextBoolean()) {
+
+			} else {
+
+			}
+		}
+		return null;
+	}
+}
